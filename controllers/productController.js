299f71a97b96f6735c869ff3209a2efdfda0191d
Wajud <wajudkareem757@gmail.com>
2024-08-13 23:16:22 +0100

    end of get all products

---
+++ controllers/productController.js
@@ -0,0 +1,13 @@
+const Product = require("../models/productModel");
+
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = { getProducts };
